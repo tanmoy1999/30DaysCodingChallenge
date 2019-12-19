6ff9c0ae8ad9951dd4ab940f39848da1056ec57f
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2019-12-19 22:47:32 +0530

    18-12-2019

---
+++ Array sum and Average(18-12-2019).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,k,i,j,sum=0,avg=0;
+    printf("Enter Num of nos (less than 100 nos): ");
+    scanf("%d",&n);
+    printf("Enter nos: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("sum: %d\n",sum);
+    avg=sum/n;
+    printf("Average: %d",avg);
+    return 0;
+}
