ddb68298704a9dbeb36e456d7ed935fb9a49410f
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2019-12-29 23:54:25 +0530

    29-12-2019

---
+++ ammending word(29-12-19).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char sou[100],des[100];
+    int n,k,i=0,l,j=0;
+    printf("Enter word: ");
+    gets(sou);
+    printf("Enter letter you want to ammend: ");
+    gets(des);
+    while(des[i]!='\0')
+    {
+        //printf("%c ",b[i]);
+        i++;
+
+    }
+    printf("%d\n",i);
+    while(sou[j]!='\0')
+    {
+        des[i]=sou[j];
+        i++;
+        j++;
+    }
+    printf("%s\n",j);
+    sou[i]='\0';
+    puts(des);
+
+    return 0;
+}
