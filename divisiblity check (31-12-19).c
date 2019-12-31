220bec8e2d7e77964981cdbaf0a21bef86432c3e
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2019-12-31 23:34:27 +0530

    Update divisiblity check (31-12-19).c

--- divisiblity check (31-12-19).c
+++ divisiblity check (31-12-19).c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+    int num,div=0,i,j,k,l;
+    printf("Enter a no. for divisiblity check: ");
+    scanf("%d",&num);
+    //printf("Range from 1 to ");
+    //scanf("%d",&k);
+    for(i=1;i<=num;i++)
+    {
+        div=num%i;
+        if(div==0)
+        {
+            printf("The no. is divisible by %d\n",i);
+        }
+    }
+    return 0;
+}
