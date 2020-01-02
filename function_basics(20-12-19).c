2603523a64b525d4eaa43ef1fc99f64a98ba0d11
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2020-01-02 23:35:38 +0530

    Update function_basics(20-12-19).c

--- function_basics(20-12-19).c
+++ function_basics(20-12-19).c
@@ -1,6 +1,6 @@
 //addition of two num using function
 
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int a,b,i,j,sum;
@@ -15,4 +15,40 @@ int add(int i,int j)
     int s=0;
     s=i+j;
     return s;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int a,b,i,j,sum;
+    printf("Enter two Number: ");
+    scanf("%d%d",&a,&b);
+    printf("a: %d b: %d\n",a,b);
+    swap(&a,&b);
+    printf("a: %d b: %d\n",a,b);
+    return 0;
+
+}
+void swap(int *a,int *b)
+{
+    int s=0;
+    s=*a;
+    *a=*b;
+    *b=s;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int a[]={1,2};
+    swap(&a[0],&a[1]);
+    printf("a: %d b: %d\n",a[0],a[1]);
+    return 0;
+
+}
+void swap(int *a[0],int *a[1])
+{
+    int s=0;
+    s=*a[0];
+    *a[0]=*a[1];
+    *a[1]=s;
 }
