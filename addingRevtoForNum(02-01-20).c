a646824da7c55ab28d9d25f6117b25c0ef413446
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2020-01-02 23:35:16 +0530

    Create addingRevtoForNum(02-01-20).c

---
+++ addingRevtoForNum(02-01-20).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int num,j,i,k,l,sum=0;
+    for(i=0,j=100;i<=100,j>=0;i++,j--)
+    {
+        sum=i*j;
+        printf("%d * %d = %d\n",i,j,sum);
+    }
+    return 0;
+}
+
+/*#include<stdio.h>
+int main()
+{
+    int num,j,i,k,l,sum=0;
+    for(i=0,j=100;i<=100,j>=0;i++,j--)
+    {
+        sum=i+j;
+        printf("%d + %d = %d\n",i,j,sum);
+    }
+    return 0;
+}*/
